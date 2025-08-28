34bd9d59829f506dc7409bd6fb35a48719567552
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-08-28 16:16:13 +0300

    update user usecase and repo

--- app/src/main/java/com/ozalp/velorasports/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/ozalp/velorasports/data/repository/UserRepositoryImpl.kt
@@ -1,35 +1,15 @@
 package com.ozalp.velorasports.data.repository
 
-import com.google.gson.Gson
 import com.ozalp.velorasports.data.remote.api.UserAPI
 import com.ozalp.velorasports.data.remote.dto.request.concretes.CreateUserRequest
 import com.ozalp.velorasports.data.remote.dto.response.abstracts.VeloraResponse
 import com.ozalp.velorasports.data.remote.dto.response.concretes.UserResponse
 import com.ozalp.velorasports.domain.repository.UserRepository
-import com.ozalp.velorasports.util.Resource
 import retrofit2.Response
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(val api: UserAPI) : UserRepository {
-    override suspend fun getUser(createUserRequest: CreateUserRequest): Resource<UserResponse> {
-        return try {
-            val response = api.getUser(createUserRequest)
-
-            if (response.isSuccessful) {
-                val body = response.body()
-                if (body != null && body.success && body.data != null) {
-                    Resource.Success(body.data)
-                } else {
-                    Resource.Error(body?.message ?: "Boş veya başarısız response")
-                }
-            } else {
-                val errorJson = response.errorBody()?.string()
-                val errorResponse = Gson().fromJson(errorJson, VeloraResponse::class.java)
-                Resource.Error(errorResponse?.message ?: "Sunucu hatası: ${response.code()}")
-            }
-        } catch (e: Exception) {
-            Resource.Error("İstisna: ${e.localizedMessage}")
-        }
+    override suspend fun getUser(createUserRequest: CreateUserRequest): Response<VeloraResponse<UserResponse>> {
+        return api.getUser(createUserRequest)
     }
-
 }
--- app/src/main/java/com/ozalp/velorasports/domain/repository/UserRepository.kt
+++ app/src/main/java/com/ozalp/velorasports/domain/repository/UserRepository.kt
@@ -3,11 +3,10 @@ package com.ozalp.velorasports.domain.repository
 import com.ozalp.velorasports.data.remote.dto.request.concretes.CreateUserRequest
 import com.ozalp.velorasports.data.remote.dto.response.abstracts.VeloraResponse
 import com.ozalp.velorasports.data.remote.dto.response.concretes.UserResponse
-import com.ozalp.velorasports.util.Resource
 import retrofit2.Response
 
 interface UserRepository {
 
-    suspend fun getUser(createUserRequest: CreateUserRequest): Resource<UserResponse>
+    suspend fun getUser(createUserRequest: CreateUserRequest): Response<VeloraResponse<UserResponse>>
 
 }
