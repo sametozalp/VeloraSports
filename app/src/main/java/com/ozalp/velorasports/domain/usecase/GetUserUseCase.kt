5564a9c0f5d5ce5d9039f35ef688550769deaac3
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-08-28 15:33:20 +0300

    update user repository and usecase

--- app/src/main/java/com/ozalp/velorasports/domain/usecase/GetUserUseCase.kt
+++ app/src/main/java/com/ozalp/velorasports/domain/usecase/GetUserUseCase.kt
@@ -12,31 +12,9 @@ import javax.inject.Inject
 
 class GetUserUseCase @Inject constructor(val userRepository: UserRepository) {
 
-    suspend operator fun invoke(createUserRequest: CreateUserRequest): Resource<User> {
-        val response: Response<VeloraResponse<UserResponse>> =
-            userRepository.getUser(createUserRequest)
-
-        if (!response.isSuccessful) {
-            val errorJson = response.errorBody()?.string()
-            val errorResponse = Gson().fromJson(errorJson, VeloraResponse::class.java)
-            return Resource.Error(errorResponse?.message ?: "Sunucu hatası: ${response.code()}")
-        }
-
-        val body = response.body()
-        if (body == null || body.data == null || !body.success) {
-            if (body?.message != null && !body.message.equals(""))
-                return Resource.Error(body.message)
-            else
-                return Resource.Error("Boş veya başarısız response")
-        }
-
-        val userResponse = body.data
-        val user = User(
-            firstName = userResponse.firstName,
-            id = userResponse.id,
-            lastName = userResponse.lastName
-        )
-
-        return Resource.Success(user)
+    suspend operator fun invoke(request: CreateUserRequest): Resource<UserResponse> {
+        val response = userRepository.getUser(request)
+        return response
     }
+
 }
